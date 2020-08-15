f03f4096d29198ebe2f287324d4632af92951f66
Hideki J <aledinai@gmail.com>
2020-08-15 06:19:08 -0300

    Add permission examples

--- examples/deno.js
+++ examples/deno.js
@@ -1,6 +1,7 @@
 const food = Deno.args[0]
 const parent = Deno.args[1]
 
+// deno run <file> <argument> <argument>...
 if (food === 'love' && parent === 'alex') {
   console.log('🦕 ... Deno is born!')
 } else {
---
+++ examples/welcomeImport.ts
@@ -0,0 +1,1 @@
+import 'https://deno.land/std@0.65.0/examples/welcome.ts'
